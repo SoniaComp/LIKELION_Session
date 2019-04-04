259292d2e6249225c9190b7474fb15ae3d3a2bc1
Sonia <syparkehsc@naver.com>
2019-04-04 11:05:35 +0900

    python2

--- python_module/module.py
+++ python_module/module.py
@@ -8,4 +8,11 @@ def mul(a, b):
   return a * b
 
 def div(a, b):
+  if b == 0:
+    print("0으로는 나눌 수 없어욧!")
+    # return "바보"
+    while(b!=0):
+      b = int(input())
+      if b == 0 :
+        print("다시 입력하세요")
   return a / b
